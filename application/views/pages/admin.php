e11f7ad4673d49bd3720b1f9c798da81aadf6a67
Jordan Cachero - ASTI <jrdncchr@gmail.com>
2014-01-27 16:58:16 +0800

    Updated Admin Page

--- application/views/pages/admin.php
+++ application/views/pages/admin.php
@@ -5,77 +5,229 @@
                 <h2>Administration</h2>
                 <hr />
                 <div class="row">
-                    <div class="col-lg-4 col-md-4" id="categoriesDiv">
-                        <h3>
-                            Categories 
-                            <button type="button" id="showAddCtgBtn" class="btn btn-success pull-right" data-toggle="modal" data-target="#newCategoryModal"><i class="fa fa-plus"></i> Add</button>
-                        </h3>
-                        <div class="table-responsive">
-                            <table cellpadding="0" cellspacing="0" border="0" class="display table table-striped" id="categories">
-                                <thead>
-                                    <tr>
-                                        <th width="10%">ID</th>
-                                        <th width="80%">Name</th>
-                                        <th width="20%">Actions</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td colspan="3" class="dataTables_empty">Loading data from server</td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                    <div class="col-lg-12 col-md-12" id="categoriesDiv">
+                        <div class="panel-group" id="accordion">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+                                        <h4 class="panel-title">
+                                            <i class="fa fa-bars"></i> Categories
+                                        </h4>
+                                    </a>
+                                </div>
+                                <div id="collapseOne" class="panel-collapse collapse">
+                                    <div class="panel-body">
+                                        <h3>
+                                            Categories 
+                                            <button type="button" id="showAddCtgBtn" class="btn btn-success pull-right" data-toggle="modal" data-target="#newCategoryModal"><i class="fa fa-plus"></i> Add</button>
+                                        </h3>
+                                        <div class="table-responsive">
+                                            <table cellpadding="0" cellspacing="0" border="0" class="display table table-striped" id="categories">
+                                                <thead>
+                                                    <tr>
+                                                        <th width="10%">ID</th>
+                                                        <th width="80%">Name</th>
+                                                        <th width="10%">Actions</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td colspan="3" class="dataTables_empty">Loading data from server</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-lg-7 col-md-7" id="usersDiv">
-                        <h3>
-                            Users 
-                        </h3>
-                        <div class="table-responsive">
-                            <table cellpadding="0" cellspacing="0" border="0" class="display table table-striped" id="users">
-                                <thead>
-                                    <tr>
-                                        <th width="20%">Username</th>
-                                        <th width="45%">Name</th>
-                                        <th width="35%">Email</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td colspan="3" class="dataTables_empty">Loading data from server</td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                </div>
+                <div style="margin-top: 10px;"></div>
+                <div class="row">
+                    <div class="col-lg-12 col-md-12" id="usersDiv">
+                        <div class="panel-group" id="accordion2">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <a data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo">
+                                        <h4 class="panel-title">
+                                            <i class="fa fa-users"></i> Users
+                                        </h4>
+                                    </a>
+                                </div>
+                                <div id="collapseTwo" class="panel-collapse collapse">
+                                    <div class="panel-body">
+                                        <h3>
+                                            Users 
+                                        </h3>
+                                        <div class="table-responsive">
+                                            <table cellpadding="0" cellspacing="0" border="0" class="display table table-striped" id="users">
+                                                <thead>
+                                                    <tr>
+                                                        <th width="20%">Username</th>
+                                                        <th width="50%">Name</th>
+                                                        <th width="30%">Email</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td colspan="3" class="dataTables_empty">Loading data from server</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
+                </div>
+                <div style="margin-top: 10px;"></div>
+                <div class="row">
+                    <div class="col-lg-12 col-md-12" id="blogsDiv">
+                        <div class="panel-group" id="accordion3">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <a data-toggle="collapse" data-parent="#accordion3" href="#collapseThree">
+                                        <h4 class="panel-title">
+                                            <i class="fa fa-book"></i> Blogs
+                                        </h4>
+                                    </a>
+                                </div>
+                                <div id="collapseThree" class="panel-collapse collapse">
+                                    <div class="panel-body">
+                                        <h3>
+                                            Blogs 
+                                            <button type="button" id="showAddCtgBtn" class="btn btn-success pull-right" data-toggle="modal" data-target="#newCategoryModal"><i class="fa fa-plus"></i> Add</button>
+                                        </h3>
+                                        <div class="table-responsive">
+                                            <table cellpadding="0" cellspacing="0" border="0" class="display table table-striped" id="categories">
+                                                <thead>
+                                                    <tr>
+                                                        <th width="10%">ID</th>
+                                                        <th width="80%">Name</th>
+                                                        <th width="20%">Actions</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td colspan="3" class="dataTables_empty">Loading data from server</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div style="margin-top: 10px"></div>
+                <div class="row">
+                    <div class="col-lg-12 col-md-12" id="categoriesDiv">
+                        <div class="panel-group" id="accordion4">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <a data-toggle="collapse" data-parent="#accordion4" href="#collapseFour">
+                                        <h4 class="panel-title">
+                                            <i class="fa fa-dot-circle-o"></i> Title Templates
+                                        </h4>
+                                    </a>
+                                </div>
+                                <div id="collapseFour" class="panel-collapse collapse">
+                                    <div class="panel-body">
+                                        <h3>
+                                            Title Templates 
+                                        </h3>
+                                        <div class="table-responsive">
+                                            <table cellpadding="0" cellspacing="0" border="0" class="display table table-striped" id="users">
+                                                <thead>
+                                                    <tr>
+                                                        <th width="20%">Username</th>
+                                                        <th width="45%">Name</th>
+                                                        <th width="35%">Email</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td colspan="3" class="dataTables_empty">Loading data from server</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div style="margin-top: 10px"></div>
+                <div class="row">
+                    <div class="col-lg-12 col-md-12" id="categoriesDiv">
+                        <div class="panel-group" id="accordion5">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <a data-toggle="collapse" data-parent="#accordion5" href="#collapseFive">
+                                        <h4 class="panel-title">
+                                            <i class="fa fa-question-circle"></i> FAQs
+                                        </h4>
+                                    </a>
+                                </div>
+                                <div id="collapseFive" class="panel-collapse collapse">
+                                    <div class="panel-body">
+                                        <h3>
+                                            FAQs 
+                                        </h3>
+                                        <div class="table-responsive">
+                                            <table cellpadding="0" cellspacing="0" border="0" class="display table table-striped" id="users">
+                                                <thead>
+                                                    <tr>
+                                                        <th width="20%">Username</th>
+                                                        <th width="45%">Name</th>
+                                                        <th width="35%">Email</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td colspan="3" class="dataTables_empty">Loading data from server</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div style="margin-top: 10px"></div>
+            </div>
+        </div>
+    </div>
+
+    <!-- New Category Modal -->
+    <div class="modal fade" id="newCategoryModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title" id="myModalLabel"><i class="fa fa-pencil"></i> <span id="ncTitle">New Category</span></h4>
+                </div>
+                <div class="modal-body">
+                    <div class="alert-danger" id="ncMessage"></div>
+                    <form role="form">
+                        <div class="form-group">
+                            <label for="title">Name</label>
+                            <input type="text" class="form-control" id="ncName" placeholder="Category Name">
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button id="ncBtn" type="button" class="btn btn-primary">Add</button>
+                    <button id="editCtgBtn" type="button" class="btn btn-primary" style="display: none;">Save</button>
                 </div>
             </div>
         </div>
     </div>
-</div>
-
-<!-- New Category Modal -->
-<div class="modal fade" id="newCategoryModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title" id="myModalLabel"><i class="fa fa-pencil"></i> <span id="ncTitle">New Category</span></h4>
-            </div>
-            <div class="modal-body">
-                <div class="alert-danger" id="ncMessage"></div>
-                <form role="form">
-                    <div class="form-group">
-                        <label for="title">Name</label>
-                        <input type="text" class="form-control" id="ncName" placeholder="Category Name">
-                    </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button id="ncBtn" type="button" class="btn btn-primary">Add</button>
-                <button id="editCtgBtn" type="button" class="btn btn-primary" style="display: none;">Save</button>
-            </div>
-        </div>
-    </div>
-</div>
